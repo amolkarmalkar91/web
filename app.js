3576a87df4b53243076680d7341cf34b47ade60d
amolkarmalkar91 <141286548+amolkarmalkar91@users.noreply.github.com>
2024-11-15 17:45:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const connection = require('./db');
 const app = express();
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
